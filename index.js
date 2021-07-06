2a62785aaf7839bc613ad2d9782baaeb06dab40a
nomadAce <raghavendra.sapaliga@signeasy.com>
2021-07-06 10:01:15 +0530

    fixed: async await issues

--- index.js
+++ index.js
@@ -9,87 +9,70 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+const TIME_LIMIT_IN_MINUTES = 30;
+
 // get_list of logged In usersID
 const Users = [
   '93',
   '109',
   '148',
-  '108',
-  '111_E Elza',
-  '101_krishna employee',
-  '112_Niranjan',
-  '113_pooja',
-  '7_krishna',
-  '98_supervisor Gadgool',
+  // '108_Ananth',
+  // '111_E Elza',
+  // '101_krishna employee',
+  // '112_Niranjan',
+  // '113_pooja',
+  // '7_krishna',
+  // '98_supervisor Gadgool',
 ];
 
 // var tasks = []
 
-// Users.forEach(user => {
+async function getUsersWorkInfo(userId) {
+  const snapshot = await database
+    .ref()
+    .child('SAM_DB')
+    .child('LOCATIONS')
+    .child(userId)
+    .orderByChild('created_at')
+    .limitToLast(1)
+    .once('value');
+  // const users = await resp.toJson();
+  const result = await snapshot.val();
 
-// Users.forEach((user) => {
-//   database
-//     .ref()
-//     .child('SAM_DB')
-//     .child('LOCATIONS')
-//     .child(user)
-//     .orderByChild('created_at')
-//     .limitToLast(1)
-//     .once('value')
-//     .then((users) => {
-//       users.forEach((element) => {
-//         const now = moment.utc();
-//         const lastLogInTime = moment(element.val().created_at);
+  if (result) {
+    // return first item of the object
+    const user = result[Object.keys(result)[0]];
 
-//         const duration = moment.duration(now.diff(lastLogInTime));
-//         // const days = duration.asDays().toFixed(2);
-//         // const hours = duration.asHours().toFixed(2);
-//         const minutes = duration.asMinutes().toFixed(2);
+    const now = moment.utc();
+    const lastLogInTime = moment(user.created_at);
 
-//         // console.log(`${user} last updated at ${element.val().created_at}`);
-//         console.log(`${user} last logged in ${minutes} minutes ago`);
-//         console.log('\n');
-//       });
-//     });
-// });
+    const duration = moment.duration(now.diff(lastLogInTime));
+    const minutes = duration.asMinutes().toFixed(2);
 
-async function getClockedInUsersInfo(clockedInUsers) {
-  const unresponsiveUsers = [];
-  await clockedInUsers.forEach((user) => {
-    database
-      .ref()
-      .child('SAM_DB')
-      .child('LOCATIONS')
-      .child(user)
-      .orderByChild('created_at')
-      .limitToLast(1)
-      .once('value')
-      .then((users) => {
-        users.forEach((element) => {
-          const now = moment.utc();
-          const lastLogInTime = moment(element.val().created_at);
+    console.log(`${user.empId} last logged in ${minutes} minutes ago`);
+    console.log('\n');
 
-          const duration = moment.duration(now.diff(lastLogInTime));
-          const minutes = duration.asMinutes().toFixed(2);
-
-          console.log(`${user} last logged in ${minutes} minutes ago`);
-          unresponsiveUsers.push(user);
-          console.log('\n');
-        });
-      });
-  });
-  console.log(unresponsiveUsers);
+    if (minutes > TIME_LIMIT_IN_MINUTES) {
+      return userId;
+    }
+  }
+  return null;
 }
 
 async function test() {
   console.log('start');
 
-  const items = await getClockedInUsersInfo(Users);
+  const promises = Users.map(async (user) => getUsersWorkInfo(user));
 
-  console.log(items);
+  const test1 = await Promise.all(promises);
+  console.log(test1);
+
   console.log('end');
 }
 
 (async () => {
+  console.time();
   await test();
+  console.timeEnd();
+  process.exit(1);
 })();
