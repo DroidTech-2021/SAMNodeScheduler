6fbc7b23948097007fafd13fbc06c140315778e1
nomadAce <raghavendra.sapaliga@signeasy.com>
2021-07-20 22:46:12 +0530

    ref: refactor code

--- index.js
+++ index.js
@@ -13,20 +13,13 @@ const database = firebase.database();
 const TIME_LIMIT_IN_MINUTES = 30;
 
 // get_list of logged In usersID
-// const Users = [
-//   '93',
-//   '109',
-//   '148',
-//   // '108_Ananth',
-//   // '111_E Elza',
-//   // '101_krishna employee',
-//   // '112_Niranjan',
-//   // '113_pooja',
-//   // '7_krishna',
-//   // '98_supervisor Gadgool',
-// ];
 
 // var tasks = []
+
+const formattedUsers = {
+  unresponsive: [],
+  active: [],
+};
 
 async function getUsersWorkInfo(userId) {
   const snapshot = await database
@@ -53,9 +46,15 @@ async function getUsersWorkInfo(userId) {
     console.log(`${user.empId} last logged in ${minutes} minutes ago`);
     console.log('\n');
 
+    // eslint-disable-next-line camelcase
+    const { created_at, address, ..._user } = user;
+
     if (minutes > TIME_LIMIT_IN_MINUTES) {
+      formattedUsers.unresponsive.push(userId);
       return userId;
     }
+    formattedUsers.active.push(_user);
+    return user;
   }
   return null;
 }
@@ -68,6 +67,17 @@ async function processUsers(users) {
   const test1 = await Promise.all(promises);
   console.log(test1);
 
+  // todo: Call API to notify supervisor
+  const { body, statusCode } = await got.post('http://localhost:5000/api/v1/zones/verifyUserLocations', {
+    json: {
+      activeUsers: formattedUsers.active,
+      unresponsiveUsers: formattedUsers.unresponsive,
+    },
+    responseType: 'json',
+  });
+  console.info(body.data);
+  console.info(statusCode);
+  console.info(formattedUsers);
   console.log('end');
 }
 
@@ -76,8 +86,27 @@ async function processUsers(users) {
   const url = 'http://localhost:5000/api/v1/timesheet/active';
   const res = await got(url);
   const result = JSON.parse(res.body);
+
+//  test
+// const Users = [
+//   '93',
+//   '109',
+//   '148',
+//   // '108_Ananth',
+//   // '111_E Elza',
+//   // '101_krishna employee',
+//   // '112_Niranjan',
+//   // '113_pooja',
+//   // '7_krishna',
+//   // '98_supervisor Gadgool',
+// ];
+
+  // const result = Users;
+
   if (result.length) {
-    await processUsers(res.body);
+    await processUsers(result);
+
+    // await processUsers(res.body);
     console.timeEnd();
   }
   process.exit(1);
